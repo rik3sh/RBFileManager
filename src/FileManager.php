39108acf5eaf212e066f28702fa45329889d5fd8
Rikesh Bajracharya <rikesh.bajracharya@gmail.com>
2022-06-20 15:43:00 +0545

    file update, delete and exists methods added

--- src/FileManager.php
+++ src/FileManager.php
@@ -11,8 +11,37 @@ class FileManager
     {
         $filename = $folder . '-' . Str::uuid() . '.' . $request->getClientOriginalExtension();
 
-        Storage::disk( config('rbfileconfig.disk') )->putFileAs( $folder, $request, $filename );
+        Storage::disk( config('rbfileconfig.disk', 'public') )->putFileAs($folder, $request, $filename);
 
         return $filename;
+    }
+
+    public static function updateFile(object $request, string $folder, string $oldFile = null)
+    {
+        if (!empty($oldFile)) self::deleteFile($folder, $oldFile);
+
+        $fileName = $folder . '-' . Str::uuid() . '.' . $request->getClientOriginalExtension();
+        
+        Storage::disk( config('rbfileconfig.disk', 'public') )->putFileAs(
+            $folder, $request, $fileName
+        );
+
+        return $fileName;
+    }
+
+    public static function deleteFile(string $folder, string $filename)
+    {
+        $filePath = $folder . '/' . $filename;
+
+        if (self::fileExists($folder, $filename)) Storage::disk( config('rbfileconfig.disk', 'public') )->delete($filePath);
+
+        return;
+    }
+
+
+    private function fileExists(string $folder, string $filename)
+    {
+        $filePath = $folder . '/' . $filename;
+        return Storage::disk( config('rbfileconfig.disk', 'public') )->exists($filePath);
     }
 }
