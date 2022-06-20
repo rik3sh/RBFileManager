e98e26b17fee6de26890f368b69a849d9cec5a88
Rikesh Bajracharya <rikesh.bajracharya@gmail.com>
2022-06-20 15:58:01 +0545

    added laravel auto package discovery

--- src/FileManagerServiceProvider.php
+++ src/FileManagerServiceProvider.php
@@ -8,9 +8,9 @@ class FileManagerServiceProvider extends ServiceProvider
 {
     public function boot() 
     {
-        $this->publishes([
-            __DIR__ . '/../config/rbfileconfig.php' => config_path('rbfileconfig.php')
-        ]);
+        $source = realpath($raw = __DIR__.'/../config/rbfileconfig.php') ?: $raw;
+
+        $this->publishes([$source => config_path('rbfileconfig.php')]);
     }
 
     public function register() 
